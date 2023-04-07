a52039a2dbbd39f64e7e101fb5c6ceec146d52e9
kaliuser13416 <38914529+kaliuser13416@users.noreply.github.com>
2023-04-07 09:24:55 -0400

    Update
    Thought of a better way to do stuff.

--- binomial.py
+++ binomial.py
@@ -1,47 +1,39 @@
 import math;
-import numpy as np;
 
 print("This is a simple program for calculating the binomial distribution.");
 print("calculate Sum(binomial distribution) (Y/n)?");
 
-once = input(":/$ " )
+once = input(": " )
 keep = {};
 index = 0;
 total = 0;
 
-def some():
-    Num = math.comb(n, i) * pow(p,i) * pow((1 - p), n-i);
-    return Num;
+print("number of trials");
+n = int(input(": " ));
+print("probability of a success");
+p = float(input(": " ));
 
 if once == "n":
-    print("number of trials");
-    n = int(input(":/$ " ));
+
     print("number of “successes”");
-    x = int(input(":/$ " ));
-    print("probability of a success");
-    p = float(input(":/$ " ));
-    Num = math.comb(n, x) * pow(p,x) * pow((1 - p), n-x);
-    print("probability is:");
-    print(Num);
+    i = int(input(": " ));
+    total = math.comb(n, i) * pow(p,i) * pow((1 - p), n-i);
+
 
 elif once == "Y":
-    
-    print("number of trials");
-    n = int(input(":/$ " ));
-    print("probability of a success");
-    p = float(input(":/$ " ));
+
     print("lower bound of “successes”");
-    i = int(input(":/$ " ));
+    i = int(input(": " ));
     print("upper bound of “successes”");
-    x = int(input(":/$ " ));
+    x = int(input(": " ));
     while (i < x):
-        keep[index] = some();
+        keep[index] = math.comb(n, i) * pow(p,i) * pow((1 - p), n-i);
         total=total+keep[index];
         i = i + 1;
         index = index + 1;
     
-    print("probability is:");
-    print(total);
-    
 else:
     print("invalid input");
+
+print("probability is:");
+print(total);
