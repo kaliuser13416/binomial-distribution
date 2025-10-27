65749461f2549addcd5bb464d6dcdf5b8271e536
tes1 <38914529+kaliuser13416@users.noreply.github.com>
2025-10-27 11:25:03 -0500

    Update binomial.py
    Refactored. Using Lambda is more concise and probably more efficient.

--- binomial.py
+++ binomial.py
@@ -1,39 +1,21 @@
-import math;
+import math
 
-print("This is a simple program for calculating the binomial distribution.");
-print("calculate Sum(binomial distribution) (Y/n)?");
+binomial = lambda n, r, p: math.comb(n, r) * pow(p, r) * pow((1-p),(n-r))
+def binomial_sum(L, U, n, p):
+    aws = []
+    while L <= U:
+        aws.append((binomial(n,L,p)))
+        L = L + 1
+    return sum(aws)
 
-once = input(": " )
-keep = {};
-index = 0;
-total = 0;
+cal_sum = input("calculate Sum(binomial distribution) (Y/n)? ")
+n = int(input("number of trials: " ))
+p = float(input("probability of a success: " ));
 
-print("number of trials");
-n = int(input(": " ));
-print("probability of a success");
-p = float(input(": " ));
-
-if once == "n":
-
-    print("number of “successes”");
-    i = int(input(": " ));
-    total = math.comb(n, i) * pow(p,i) * pow((1 - p), n-i);
-
-
-elif once == "Y":
-
-    print("lower bound of “successes”");
-    i = int(input(": " ));
-    print("upper bound of “successes”");
-    x = int(input(": " ));
-    while (i < x):
-        keep[index] = math.comb(n, i) * pow(p,i) * pow((1 - p), n-i);
-        total=total+keep[index];
-        i = i + 1;
-        index = index + 1;
-    
+if cal_sum == "Y":
+    low = int(input("lower bound of successes: "))
+    up = int(input("upper bound of successes: "))
+    print(binomial_sum(low, up, n, p))
 else:
-    print("invalid input");
-
-print("probability is:");
-print(total);
+    r = int(input("number of successes: "))
+    print(binomial(n,r,p))
