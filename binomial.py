2ab030df0f21f8d25e4e5ec0a031b60d7414c047
tes1 <38914529+kaliuser13416@users.noreply.github.com>
2025-11-05 09:49:42 -0600

    Update binomial.py
    Changed the binomial_sum function into a single Lambda, rather than a function that loops calling the binomial Lambda.

--- binomial.py
+++ binomial.py
@@ -1,21 +1,13 @@
-import math
-
-binomial = lambda n, r, p: math.comb(n, r) * pow(p, r) * pow((1-p),(n-r))
-def binomial_sum(L, U, n, p):
-    aws = []
-    while L <= U:
-        aws.append((binomial(n,L,p)))
-        L = L + 1
-    return sum(aws)
-
-cal_sum = input("calculate Sum(binomial distribution) (Y/n)? ")
+from math import comb
+cal_sum = str(input("calculate Sum(binomial distribution) (Y/n)? "))
 n = int(input("number of trials: " ))
 p = float(input("probability of a success: " ));
-
+binomial = lambda n, r, p: comb(n, r) * (p ** r) * ((1 - p) ** (n - r))
+binomial_sum = lambda L, U, n, p: sum(comb(n, k) * (p ** k) * ((1 - p) ** (n - k)) for k in range(L, U + 1))
 if cal_sum == "Y":
-    low = int(input("lower bound of successes: "))
-    up = int(input("upper bound of successes: "))
-    print(binomial_sum(low, up, n, p))
+    L = int(input("lower bound of successes: "))
+    U = int(input("upper bound of successes: "))
+    print(binomial_sum(L,U,n,p))
 else:
     r = int(input("number of successes: "))
     print(binomial(n,r,p))
