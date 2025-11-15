ef6a58b57e67705d2db6d6138fd1775baa16136c
tes1 <38914529+kaliuser13416@users.noreply.github.com>
2025-11-14 21:20:44 -0600

    Update binomial.py
    Made some minor changes

--- binomial.py
+++ binomial.py
@@ -1,9 +1,9 @@
 from math import comb
 cal_sum = str(input("calculate Sum(binomial distribution) (Y/n)? "))
 n = int(input("number of trials: " ))
-p = float(input("probability of a success: " ));
+p = float(input("probability of a success: " ))
 binomial = lambda n, r, p: comb(n, r) * (p ** r) * ((1 - p) ** (n - r))
-binomial_sum = lambda L, U, n, p: sum(comb(n, k) * (p ** k) * ((1 - p) ** (n - k)) for k in range(L, U + 1))
+binomial_sum = lambda L, U, n, p: sum(comb(n, r) * (p ** r) * ((1 - p) ** (n - r)) for r in range(L, U + 1))
 if cal_sum == "Y":
     L = int(input("lower bound of successes: "))
     U = int(input("upper bound of successes: "))
@@ -11,3 +11,4 @@ if cal_sum == "Y":
 else:
     r = int(input("number of successes: "))
     print(binomial(n,r,p))
+
